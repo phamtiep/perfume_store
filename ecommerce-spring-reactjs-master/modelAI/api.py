90f1a6b8e2808390184eb1c3d1d2aab66dc1aa6f
VietAnh-AI2-UET <144088322+VietAnh-AI2-UET@users.noreply.github.com>
2024-05-26 05:19:50 +0700

    Update api.py
    change api

--- ecommerce-spring-reactjs-master/modelAI/api.py
+++ ecommerce-spring-reactjs-master/modelAI/api.py
@@ -1,51 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api
-import torch
-from sklearn import preprocessing
-import pandas as pd
-from model import RecSysModel, PerfumeDataset, predict
 from concat import author2id, merged_df
 import numRate
 from popularity import popular_perfume_id
+from flask_cors import CORS
+import model
+from model import df, svd
 
 # Initialize Flask app
 app = Flask(__name__)
 api = Api(app)
 
-def load_model():
-    model = RecSysModel(num_users=0, num_perfumes=0)  # Initialize model
-    model.load_state_dict(torch.load('model.pth'))
-    return model
-
-# Initialize label encoders
-def initialize_label_encoders(df):
-    lbl_user = preprocessing.LabelEncoder()
-    lbl_perfume = preprocessing.LabelEncoder()
-
-    lbl_user.fit(df['author_id'])
-    lbl_perfume.fit(df['perfume_id'])
-
-    return lbl_user, lbl_perfume
-
-# Route to recommend perfumes
 class Recommend(Resource):
     def get(self, author):
         user_id = author2id.get(author)
         nRate = numRate.count_perfumes_for_author(merged_df, user_id)
         if (nRate < 3):
-            return jsonify({'recommendations': popular_perfume_id})
+            return popular_perfume_id
         else:
-            # Load model and label encoders
-            model = load_model()
-            lbl_user, lbl_perfume = initialize_label_encoders(merged_df)
-    
-            # Generate recommendations
-            recommendations = predict(user_id, model, lbl_user, lbl_perfume)
-    
-            # Return recommendations
-            return jsonify({'recommendations': recommendations})
-    
+            return model.recommend_items(user_id, svd, df).tolist()
+        
 api.add_resource(Recommend, '/recommend/<string:author>')
+CORS(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
